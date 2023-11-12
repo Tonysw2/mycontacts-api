2eb5ea427755124c1ad42a40617a22965e38504d
Tonysw2 <anthonyrmayaa@gmail.com>
2023-11-12 11:25:32 -0300

    feat: implement all routes, controllers and repositories to create, read, update and delete

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -7,20 +7,81 @@ class ContactController {
     res.json(contacts);
   }
 
-  show() {
-    // Obter um registro
+  async show(req, res) {
+    const { id } = req.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    return res.json(contact);
   }
 
-  store() {
-    // Criar um novo registro
+  async store(req, res) {
+    const {
+      name, email, phone, category_id,
+    } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return res.status(400).json({ error: 'This email already exists' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    return res.send(contact);
   }
 
-  update() {
-    // Editar um registro
+  async update(req, res) {
+    const { id } = req.params;
+    const {
+      name, email, phone, category_id,
+    } = req.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return res.status(400).json({ error: 'Contact not found' });
+    }
+
+    if (!name) {
+      return res.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return res.status(400).json({ error: 'This email is already in use.' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    return res.json(contact);
   }
 
-  delete() {
-    // Deletar um registro
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    await ContactsRepository.delete(id);
+
+    return res.sendStatus(204);
   }
 }
 
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,11 +1,17 @@
 const crypto = require('crypto');
 
-const contacts = [
+let contacts = [
   {
     id: crypto.randomUUID(),
     name: 'Anthony',
     email: 'anthony@email.com',
     phone: '1212121212',
+    category_id: crypto.randomUUID(),
+  }, {
+    id: crypto.randomUUID(),
+    name: 'Matheus',
+    email: 'matheus@email.com',
+    phone: '1414141414',
     category_id: crypto.randomUUID(),
   },
 ];
@@ -14,6 +20,61 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.email === email));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+
+  create({
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: crypto.randomUUID(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts.push(newContact);
+
+      resolve(newContact);
+    });
+  }
+
+  update(id, {
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts = contacts.map((contact) => (contact.id === id ? updatedContact : contact));
+
+      resolve(updatedContact);
     });
   }
 }
