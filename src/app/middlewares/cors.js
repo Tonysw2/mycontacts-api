b7eec3b0ef5255d0fe91f28889d6a3be55b7a218
Tonysw2 <anthonyrmayaa@gmail.com>
2023-11-22 22:32:59 -0300

    feat: add headers to cors

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,4 +1,7 @@
 module.exports = function cors(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Max-Age', '10');
   next();
 };
